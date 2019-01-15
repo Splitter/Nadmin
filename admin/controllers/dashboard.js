74ae751c710512bbd32495dbc417de5644a697e6
Splitter <splitvisionz@gmail.com>
2019-01-14 20:19:00 -0500

    Beginning of admin dashboard
    main dashboard index functionality

---
+++ admin/controllers/dashboard.js
@@ -0,0 +1,53 @@
+
+const   express       = require('express')
+const   dashboard     = express.Router()
+
+//Internal Error statement
+const   serverErrorStatement = "There was an error in your request. Please try again later."
+
+
+dashboard.get("/",(req,res)=>{
+        res.locals.nadminDashModels = []
+        if(Array.isArray(dashboard.pageArray) ){
+                let pageCount = 0
+                dashboard.pageArray.forEach((page) => {
+                        page.object.model.find({}, (err, docs)=>{
+                                if ( err ){                                
+                                    return res.status(500).send(serverErrorStatement)
+                                }   
+                                if(docs){
+                                        res.locals.nadminDashModels.push({
+                                                name:page.name,
+                                                count:docs.length,
+                                                dispOrder: page.object.settings.displayOrder
+                                        })
+                                }
+                                else{
+                                        res.locals.nadminDashModels.push({
+                                                name:page.name,
+                                                count:0,
+                                                dispOrder: page.settings.displayOrder
+                                        })
+                                }
+                                pageCount ++
+                                if ( pageCount >= dashboard.pageArray.length){
+                                        res.locals.nadminDashModels.sort((a,b)=>{
+                                            if(a.dispOrder < b.dispOrder){
+                                                return -1
+                                            }
+                                            if(a.dispOrder > b.dispOrder){
+                                                return 1
+                                            }
+                                            return 0
+                                        })
+                                        res.render("index")     
+                                }                                
+                        })
+                })              
+        }
+        else{
+                return res.status(500).send("ERROR: no page models available")
+        }
+})
+
+module.exports= dashboard 
--- admin/lib/adminApp.js
+++ admin/lib/adminApp.js
@@ -1,55 +1,68 @@
 const   express   = require("express"),
-        helmet    = require("helmet"),
+        helmet    = require("helmet")(),
         glob = require( 'glob' ),
         path = require( 'path' ),
         helmetCSP = require("../../lib/utilities").helmetCSP
+
+const   dashboard = require("../controllers/dashboard")
 //adminApp is just an express object extended and manipulated
 adminApp = express()
-
-//override app.render so we dont have to pass full path to each view in res.render
-//if we attempt to use adminApp.set('views',PATH) setting is overridden by main app so work around it
-adminApp.nRender = adminApp.render;
-adminApp.render = (name, options, callback)=>{
-    adminApp.nRender(__dirname + "/../views/"+name,options,callback)
-}
-
+//easy access to admin root url and helmet protections
+adminApp.use("/", (req, res, next)=>{
+        res.locals.nadminAdminURL = req.protocol + "://" + req.headers.host + req.nadminSettings.adminRoute
+        helmet(req,res,next)
+})
 //Content Security policy middleware
 adminApp.use(helmetCSP) 
-
-//make static nadmin admin assets accessable
-adminApp.use("/public",express.static(__dirname + "/../public"))
+    
 
 /**
- * Utility function to initialize a few things
+ * Initialize adminApp 
  *
  * @param {Object} options
  */
 adminApp.init = ( options ) => {
-    //allow disabling of general helmet middleware, incase custom settings are used in main site
-    //This keeps the CSP protection intact seperately for Nadmin routes
-    if(options.enableHelmet){
-        adminApp.use(helmet())
-    }
+    //get page array if non-existant
     if(!Array.isArray(adminApp.pageArray) || !adminApp.pageArray.length){
-        adminApp.getPages(options)
+        adminApp.setupPages(options)
     }
+    //Output well formatted html
+    adminApp.locals.pretty = true
+    //set admin dashboard title
+    adminApp.locals.nadminAdminTitle = options.adminTitle
+
+    //override app.render so we dont have to pass full path to each view in res.render
+    //if we attempt to use adminApp.set('views',PATH) the setting is overridden by main app so work around it
+    adminApp.nRender = adminApp.render;
+    adminApp.render = (name, options, callback)=>{
+        adminApp.nRender(__dirname + "/../views/"+name,options,callback)
+    }    
+    
+    //make static nadmin admin assets accessable
+    adminApp.use("/public",express.static(__dirname + "/../public"))
 }
 
 /**
- * Gets all page models into array
+ * Gets all page models into array and direct routes to page 
  * 
  */
-adminApp.getPages = ( options ) => {
+adminApp.setupPages = ( options ) => {
     const page = require("./../../lib/page")
     const modelPath = options.appRoot + "/"+ options.modelDirectory;
     adminApp.pageArray = []
+    adminApp.locals.nadminAdminLinks = []
     glob.sync(modelPath+'/*.js').forEach( function( file ) {
         try {
             let nPage =  require( path.resolve( file ) )
             if (nPage instanceof page){
+                //collect array of pages for rendering admin index and navigation
                 adminApp.pageArray.push({
-                    name : path.basename(file, '.js'),
-                    class: nPage
+                    name : nPage.name,
+                    object: nPage
+                })
+                //route /name to 'route' method of page object
+                adminApp.use(("/"+nPage.name), (req, res)=>{
+                    nPage.route(req,res)
                 })
             }
         }
@@ -57,6 +70,23 @@ adminApp.getPages = ( options ) => {
             console.log(e)
         }
     });
+    adminApp.pageArray.sort((a,b)=>{
+        if(a.object.settings.displayOrder < b.object.settings.displayOrder){
+            return -1
+        }
+        if(a.object.settings.displayOrder > b.object.settings.displayOrder){
+            return 1
+        }
+        return 0
+    })
+    adminApp.pageArray.forEach((nPage)=>{    
+        adminApp.locals.nadminAdminLinks.push(nPage.name)
+    })
+    dashboard.pageArray = adminApp.pageArray
+    
 }
+
+adminApp.use("/",dashboard)
+
 
 module.exports= adminApp
