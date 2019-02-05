1f8b04c09c32df19a889758bbfb178693d36d5cf
Splitter <splitvisionz@gmail.com>
2019-02-05 10:52:38 -0500

    Add localization to dashboard index

--- admin/controllers/dashboard.js
+++ admin/controllers/dashboard.js
@@ -2,38 +2,31 @@
 const   express       = require('express')
 const   dashboard     = express.Router()
 
-//Internal Error statement
-const   serverErrorStatement = "There was an error in your request. Please try again later."
-
 
 dashboard.get("/",(req,res)=>{
         res.locals.nadminDashModels = []
         if(Array.isArray(dashboard.pageArray) ){
+                //Loop through pages to get data for dashboard index
                 let pageCount = 0
                 dashboard.pageArray.forEach((page) => {
+                        //For each page type see how many records there are
                         page.object.model.find({}, (err, docs)=>{
                                 if ( err ){                                
-                                    return res.status(500).send(serverErrorStatement)
+                                    return res.status(500).send(res.__("There was an error in your request. Please try again later."))
                                 }   
-                                if(docs){
-                                        res.locals.nadminDashModels.push({
-                                                name:page.name,
-                                                count:docs.length,
-                                                canCreate: page.object.settings.canCreate,
-                                                dispOrder: page.object.settings.displayOrder
+                                //Add data to locals befor rendering index
+                                res.locals.nadminDashModels.push({
+                                        name:page.name,
+                                        count:(docs ? docs.length : 0),
+                                        canCreate: page.object.settings.canCreate,
+                                        dispOrder: page.object.settings.displayOrder
 
-                                        })
-                                }
-                                else{
-                                        res.locals.nadminDashModels.push({
-                                                name:page.name,
-                                                count:0,
-                                                canCreate: page.object.settings.canCreate,
-                                                dispOrder: page.settings.displayOrder
-                                        })
-                                }
+                                })
+                                
                                 pageCount ++
+                                //If we are at the last item in loop
                                 if ( pageCount >= dashboard.pageArray.length){
+                                        //Sort items by their dispOrder option
                                         res.locals.nadminDashModels.sort((a,b)=>{
                                             if(a.dispOrder < b.dispOrder){
                                                 return -1
@@ -43,13 +36,14 @@ dashboard.get("/",(req,res)=>{
                                             }
                                             return 0
                                         })
+                                        //render index page
                                         res.render("index")     
                                 }                                
                         })
                 })              
         }
         else{
-                return res.status(500).send("ERROR: no page models available")
+                return res.status(500).send(res.__("ERROR: no page models available"))
         }
 })
 
